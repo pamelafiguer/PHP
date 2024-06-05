02ff9dc853570124a1e174a4852ed6849dc47bd9
= <=>
2024-06-05 15:29:38 -0500

    php

---
+++ ejemplo01.php
@@ -0,0 +1,6 @@
+// etiqueta de inicio
+
+<?php
+echo "Bienvenido al mundo de php";
+
+?> // etiqueta de cierre
--- ejemplo02.php
+++ ejemplo02.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <title>Mi pagina</title>
 </head>
 <body>
     <h1> <?php echo "Bienvenido al mundo de php";?></h1>
     <h2> <?php echo "Lenguaje de programacion";?></h2>
     <?php
-    echo "Laragon es una aplicacion para el servidor web<br>;"; 
+    echo "Laragon es una aplicacion para el servidor web <br>";
+    echo "Laragon es una aplicacion que contiene PHP, MYADMIN, MYSQL <br>"; 
     ?>
     
 </body>
