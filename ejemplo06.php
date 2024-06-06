5fd7dcb85d1e0f057099628955dfb5f799514254
@pamelafiguer <pamelaestefani2005@gmail.com>
2024-06-06 08:53:06 -0500

    php

---
+++ ejemplo06.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_POST)){
+$n1=$_POST['txtCategoria'];
+$n2=$_POST['txtSueldo'];
+}
+
+
+$n1= 20;
+$n2= 3;
+
+
+echo "El numero 1 es : $n1 <br>";
+echo "El numero 2  es : $n2 <br>";
+echo "-------------------------------------<br>";
+echo "La Multiplicacion es : $Multiplicacion<br>";
+echo "La  Division es : $division <br>";
+echo "El residuo es : $residuo <br>";
+echo "La Potencia es : $Potencia <br>";
+
+?>
